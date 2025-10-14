6a4e5e6e3ae6e4efa30ee5a1ac013f7d82aed628
Matheus Duarte <teusinhoduarte@gmail.com>
2025-10-14 11:19:13 -0300

    Primeiro commit

---
+++ src/main/java/br/sp/fatec/product/entities/Product.java
@@ -0,0 +1,43 @@
+package br.sp.fatec.product.entities;
+   
+public class Product {   
+
+    private long id;
+    private String name;
+    private String description;
+    private Double price;
+
+    public Product(long id, String name, String description, Double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    
+}
+  
